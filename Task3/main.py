5cb01e0b3a71f55329b33fbf6b77081c1ffedeca
Элина <elinamakipova@gmail.com>
2024-01-17 15:24:12 +0600

    updated version of this code

--- Task3/main.py
+++ Task3/main.py
@@ -1,8 +1,18 @@
-help please
+result = {}
+with open('data/data.txt', 'r') as file:
+    for i in file:
+        for each_line in i:
+            if each_line.isdigit():
+                if each_line in result:
+                    result[each_line] += 1
+                else:
+                    result[each_line] = 1
 
-# Выше должен быть твой код(результат выполнения записать в переменную result)
-# Код ниже ты не должна менять!
-if result == {1: 7, 2: 6, 3: 5, 4: 3, 5: 2}:
+if result == {'1': 7, '2': 6, '3': 5, '4': 3, '5': 2}:
     print("Ты молодец")
 else:
     print("Ещё нет")
+
+
+
+
