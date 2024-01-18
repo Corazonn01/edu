20d813169371c451bc165d7e1e507e7f884fdf3a
Элина <elinamakipova@gmail.com>
2024-01-18 10:35:15 +0600

    revised plan and code (added char and saw  if all of the elements from the dict are integers)

--- Task3/main.py
+++ Task3/main.py
@@ -3,15 +3,17 @@ with open('data/data.txt', 'r') as file:
     for i in file:
         for each_line in i:
             if each_line.isdigit():
-                if each_line in result:
-                    result[each_line] += 1
+                char = int(each_line)
+                if char in result:
+                    result[char] += 1
                 else:
-                    result[each_line] = 1
+                    result[char] = 1
 
 if result == {1: 7, 2: 6, 3: 5, 4: 3, 5: 2}:
     print("Ты молодец")
 else:
     print("Ещё нет")
+
 
 
 
